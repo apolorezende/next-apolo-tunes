a520f638192685f48786c2da7f8cb9f458e51f31
apolorezende <apolo_henriquerr@hotmail.com>
2024-02-15 17:31:43 -0300

    feat: funcoes get api entre outras criadas

--- src/pages/api/searchAlbumsAPI.tsx
+++ src/pages/api/searchAlbumsAPI.tsx
@@ -1,4 +1,4 @@
-import AlbumData from "../../interfece/Interface"
+import AlbumData from "../../interface/Interface"
 
 const searchAlbumsAPI = async (artist: any) => {
   const artistNameURL = encodeURI(artist).replaceAll('%20', '+');
